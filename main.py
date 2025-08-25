727e65d91f233ea428ade15ecbf1238c37c12605
Allana Lima <lima.marques10@aluno.ifce.edu.br>
2025-08-25 21:54:09 +0000

    descrição das alterações realizadas

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI(title='API de Gabriela e Allana')
 @app.get("/")
 def hello():
      return{"message": "Hello World!"}
+
+@app.get("/aluno")
+def get_aluno():
+     return{"insira a rota após a '/' o nome do aluno"}
+
+@app.get("/aluno/{nome_aluno}")
+def get_aluno_by_name(nome_aluno:str):
+     return{"o nome do aluno é": nome_aluno}
